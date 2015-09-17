bd3a09740f2c9a10476fc86601386644db5f4c6a
CaptainLunar <jeanmalmonte@yahoo.com>
2015-09-17 10:37:59 -0500

    Create feels.hs

---
+++ feels.hs
@@ -0,0 +1,22 @@
+module Feels where
+
+feelsGoodMan :: String
+feelsGoodMan = "Feels good man"
+
+feelsBadMan :: String
+feelsBadMan = "Feels bad man"
+
+iktf :: String
+iktf = "I know that feel"
+
+idktf :: String
+idktf = "I don't know that feel"
+
+feels :: String -> String
+feels mood = "Feels " ++ mood ++ " man"
+
+ree :: String
+ree = "REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
+
+anthony :: String
+normies = "FUCKING NORMALFAGS, GET THE FUCK OFF MY BOARD"
